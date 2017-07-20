57da5a18c73495362d122d34719233b154d47978
yukonboy <yukonboy@telus.net>
2017-07-20 18:49:33 +0000

    Make the commit button functional to save the comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    
+    RATINGS = {
+        'one star': '1_star',
+        'two star': '2_stars',
+        'three star': '3_stars',
+        'four star': '4_stars',
+        'five star': '5_stars'
+    }
+    
 end
