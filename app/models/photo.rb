df6460948d56463f7eeee4b2fa9d49888f6b70cd
yukonboy <yukonboy@telus.net>
2017-08-15 13:30:45 +0000

    Another test of working Dev app on Heroku

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   #paginates_per 4  # Not sure that this will work but in here to try it out - Guy
-  #belongs_to :user
+  belongs_to :user
   belongs_to :place
   
   mount_uploader :picture, PictureUploader
